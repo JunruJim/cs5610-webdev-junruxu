3009803cd6c39c2515773a7a3d573f1a413286a7
JunruJim <lebenbot@gmail.com>
2018-03-15 15:53:34 -0700

    assignment4: seems works

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Point static path to dist -- For building -- REMOVE
 app.use(express.static(path.join(__dirname, 'dist')));
@@ -22,7 +22,6 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
 
@@ -35,11 +34,6 @@ const server = http.createServer(app);
 //var serverSide = require("./server/test-mongodb/app");
 //serverSide(app);
 
-// For Build: Catch all other routes and return the index file -- BUILDING
-app.get('*', function (req, res) {
-  // res.sendFile(path.join(__dirname, 'dist/index.html'));
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
 
 
 // var hello = require('./hello');
@@ -47,6 +41,13 @@ app.get('*', function (req, res) {
 // or require('./hello')(app);
 
 require("./assignment/app")(app);
+
+
+// For Build: Catch all other routes and return the index file -- BUILDING
+app.get('*', function (req, res) {
+  // res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 
 server.listen( port , function() {
   console.log('Node app is running on port', app.get('port'))
