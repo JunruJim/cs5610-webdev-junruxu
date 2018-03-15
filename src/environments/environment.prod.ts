88ec26b1b1a17d11b9a3c4c889f9a90347682968
JunruJim <lebenbot@gmail.com>
2018-03-15 14:20:20 -0700

    refresh test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://cs5610-webdev-junruxu.herokuapp.com',
-  baseUrlClient: 'https://cs5610-webdev-junruxu.herokuapp.com'
+  baseUrl: 'https://cs5610-webdev-junruxu.herokuapp.com/',
+  baseUrlClient: 'https://cs5610-webdev-junruxu.herokuapp.com/'
 };
