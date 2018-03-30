70a139f44ab27575747fe1aa8e748fb80f89e81c
JunruJim <lebenbot@gmail.com>
2018-03-30 11:56:00 -0700

    assignment5: testing after db connection done

--- assignment/model/models.server.js
+++ assignment/model/models.server.js
@@ -4,15 +4,16 @@ var mongoose = require('mongoose');
 // mongodb://heroku_jmhwjlk9:11pl3qm17i1j15v150ifp6ruc1@ds111648.mlab.com:11648/heroku_jmhwjlk9
 // mongodb://admin:admin@ds111648.mlab.com:11648/heroku_jmhwjlk9
 
-// var connectionString = 'mongodb://localhost:27017/webdev'; // for local
-// if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-//   var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-//   var password = process.env.MLAB_PASSWORD_WEBDEV;
-//   connectionString = 'mongodb://' + username + ':' + password;
-//   connectionString += '@ds111648.mlab.com:11648/heroku_jmhwjlk9/webdev'; // use yours
-// }
+var connectionString = 'mongodb://localhost:27017/webdev'; // for local
+if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+  var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+  var password = process.env.MLAB_PASSWORD_WEBDEV;
+  connectionString = 'mongodb://' + username + ':' + password;
+  connectionString += '@ds111648.mlab.com:11648/heroku_jmhwjlk9/webdev'; // use yours
+}
 
 
-var db = mongoose.connect('mongodb://admin:admin@ds111648.mlab.com:11648/heroku_jmhwjlk9', {useMongoClient: true});
+// var db = mongoose.connect('mongodb://admin:admin@ds111648.mlab.com:11648/heroku_jmhwjlk9', {useMongoClient: true});
+var db = mongoose.connect(connectionString, {useMongoClient: true});
 
 module.exports = db;
